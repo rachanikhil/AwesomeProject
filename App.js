d38593ada19114d669cb059c126b2b5148eb5508
Nikhil Rachawar <nikhil123rachawar@gmail.com>
2022-10-03 11:26:27 +0530

    start of react Native

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View>
+        <Text style={styles.dummyText}>This is Nikhil</Text>
+      </View>
+      <Text style={styles.dummyText}>Hello World!!!</Text>
+      <Button title="New Button" />
+    </View> 
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center", 
+    justifyContent: "center",
+  }, 
+  dummyText: {
+    margin: 16,
+    borderWidth: 1,
+    borderColor: "red",
+    padding: 16,
   },
 });
